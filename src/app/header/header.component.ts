f69caafee0582402cd1140eeec56b5370ece335b
kanpizza <kanpizza@Kanpichchas-Macbook-Pro.local>
2018-11-29 23:28:15 +0530

    Complete UID of Login and SignUp page with responsive

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,40 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component } from '@angular/core';
+import { MatDialog } from '@angular/material';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 
-export class HeaderComponent implements OnInit {
+export class HeaderComponent  {
+  constructor(public dialog: MatDialog) {}
 
-  constructor() { }
+  signUp_dialog() {
+    const dialogRef = this.dialog.open(SignUpComponent, {panelClass: 'myapp-no-padding-dialog'});
 
-  ngOnInit() {
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
   }
+  logIn_dialog() {
+    const dialogRef = this.dialog.open(LogInComponent, {panelClass: 'myapp-no-padding-dialog'});
 
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
 }
+@Component({
+  selector: 'app-log-in',
+  templateUrl: '../w-space/log-in/log-in.component.html',
+  styleUrls: ['../w-space/log-in//log-in.component.scss']
+})
+export class LogInComponent {}
+
+@Component({
+  selector: 'app-sign-up',
+  templateUrl: '../w-space/sign-up/sign-up.component.html',
+  styleUrls: ['../w-space/sign-up/sign-up.component.scss']
+})
+export class SignUpComponent {}
+
--- src/app/w-space/log-in/log-in.component.ts
+++ src/app/w-space/log-in/log-in.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog } from '@angular/material';
 
 @Component({
   selector: 'app-log-in',
