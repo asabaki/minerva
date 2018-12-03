f67a6103b5d4267d0b5f0248c547f66f5fc5d183
Asi Baka <39007861+asabaki@users.noreply.github.com>
2018-12-03 20:44:56 +0530

    Complete Simple User Feature (#14)

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,28 +1,57 @@
-import { Component } from '@angular/core';
-import { MatDialog } from '@angular/material';
+import {Component, OnInit} from '@angular/core';
+import {MatDialog} from '@angular/material';
 import {SignUpComponent} from '../w-space/sign-up/sign-up.component';
 import {LogInComponent} from '../w-space/log-in/log-in.component';
+import {AuthService} from '../w-space/services/auth.service';
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
 
-export class HeaderComponent  {
-  constructor(public dialog: MatDialog) {}
+export class HeaderComponent implements OnInit {
+  name: string;
+  id: string;
+  isAuth: boolean;
+
+  constructor(public dialog: MatDialog,
+              public authService: AuthService) {
+  }
+
+  ngOnInit() {
+    this.isAuth = this.authService.getIsAuth();
+    this.name = this.authService.getUserName();
+    this.authService.getAuthStatus().subscribe((res) => {
+      console.log('Login: ' + res);
+      this.name = this.authService.getUserName();
+      this.isAuth = res;
+    });
+  }
 
   signUp_dialog() {
     const dialogRef = this.dialog.open(SignUpComponent, {panelClass: 'myapp-no-padding-dialog'});
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
     });
   }
+
   logIn_dialog() {
     const dialogRef = this.dialog.open(LogInComponent, {panelClass: 'myapp-no-padding-dialog'});
 
     dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
     });
+  }
+
+  getUser() {
+    return this.authService.getUserName();
+  }
+
+  isLogin() {
+    return this.isAuth;
+  }
+
+  onLogout() {
+    this.authService.logout();
   }
 }
--- src/app/w-space/log-in/log-in.component.ts
+++ src/app/w-space/log-in/log-in.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
-
-import { MatDialog } from '@angular/material';
-
+import {Component, Inject, Injectable, OnInit} from '@angular/core';
+import {MAT_SNACK_BAR_DATA, MatDialog, MatDialogRef, MatSnackBar} from '@angular/material';
+import {NgForm} from '@angular/forms';
+import {AuthService} from '../services/auth.service';
+import {SuccessSnackComponent} from '../sign-up/sign-up.component';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-log-in',
@@ -10,9 +12,57 @@ import { MatDialog } from '@angular/material';
 })
 export class LogInComponent implements OnInit {
 
-  constructor() { }
+  constructor(private authService: AuthService,
+              private dialogRef: MatDialogRef<LogInComponent>,
+              private matSnack: MatSnackBar,
+              private router: Router) { }
 
   ngOnInit() {
   }
+  onLogin(f: NgForm) {
+    // console.log(f.value.email);
+    this.authService.login(f.value.email, f.value.password).subscribe((res) => {
+      // console.log(res);
+      if (res.error) {
+        this.matSnack.openFromComponent(ErrorLoginSnackComponent, {
+          data: res.error.message,
+          duration: 1500,
+          panelClass: 'my-snack'
+        });
+      } else {
+        this.matSnack.openFromComponent(SuccessSnackComponent, {
+          data: 'Login Success!',
+          duration: 1500,
+          panelClass: 'my-snack'
+        });
+        this.router.navigate(['/']);
+        this.dialogRef.close();
+      }
+    });
+  }
 
+}
+
+@Component({
+  selector: 'error-snack',
+  templateUrl: 'error-snack.html',
+  styles: [`
+    .error-snack {
+      color: white;
+      font-size: 15px;
+      justify-content: center;
+      display: flex;
+      align-items: center;
+      padding: auto;
+    }
+
+    .icon {
+      color: red;
+      font-weight: 700;
+    }
+  `],
+})
+export class ErrorLoginSnackComponent {
+  constructor(@Inject(MAT_SNACK_BAR_DATA) public data: any) {
+  }
 }
