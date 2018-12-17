32511f1cbc291270b1bfd4900c83f8d7c103578a
Asi Baka <39007861+asabaki@users.noreply.github.com>
2018-12-17 14:44:04 +0530

    Asi branch (#21)
    * Database re-config
    
    * Add Flash Card Fetching Feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import {WSpaceComponent} from './w-space/w-space.component';
 import {AuthGuard} from './w-space/services/auth.guard';
 import {FlashcardComponent} from './w-space/flashcard/flashcard.component';
+import {CollectionComponent} from './w-space/flashcard/collection/collection.component';
 
 const routes: Routes = [
   {path: '', component: WSpaceComponent , pathMatch: 'full'},
   {path: 'flash', component: FlashcardComponent},
+  {path: 'flash/:id', component: CollectionComponent},
 ];
 
 @NgModule({
