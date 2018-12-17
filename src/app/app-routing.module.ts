5cc128d5cab071c6f76e26ebbec5188d72284381
kanpizza <kanpizza@Kanpichchas-Macbook-Pro.local>
2018-12-17 14:48:47 +0530

    Add Some Quiz Feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import {WSpaceComponent} from './w-space/w-space.component';
 import {AuthGuard} from './w-space/services/auth.guard';
 import {FlashcardComponent} from './w-space/flashcard/flashcard.component';
+import {QuizComponent} from './w-space/quiz/quiz.component';
 
 const routes: Routes = [
   {path: '', component: WSpaceComponent , pathMatch: 'full'},
   {path: 'flash', component: FlashcardComponent},
+  {path: 'quiz', component: QuizComponent},
 ];
 
 @NgModule({
